7f78876dac2a41ef44387ec91d3ea80c285bbadb
tirth-2510 <iamtirth2510@gmail.com>
2025-07-04 15:22:37 +0530

    change: removed vector DB connection class

--- services/dbresponse.py
+++ services/dbresponse.py
@@ -1,33 +1,13 @@
 import os
 from dotenv import load_dotenv
-from langchain_milvus import Zilliz
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from pymongo import MongoClient
-from utils.text_formatter import Formatter
 
 # Load environment variables
 load_dotenv()
 
-embeddings = GoogleGenerativeAIEmbeddings(model="models/text-embedding-004", google_api_key=os.getenv("GOOGLE_API_KEY"))
-
 mongoClient = MongoClient(os.getenv("MONGODB_URI"))
 db=mongoClient["sdp_chatbot"]
 collection=db["foodItems"]
-
-class milvusDB():
-    @staticmethod
-    def vectorstore(document_id: str):
-        return Zilliz(
-            collection_name=document_id,
-            connection_args={"uri": os.getenv("ZILLIZ_URI_ENDPOINT"), "token": os.getenv("ZILLIZ_TOKEN")},
-            index_params={"index_type": "IVF_PQ", "metric_type": "COSINE"},
-            embedding_function=embeddings,
-        )
-            
-    @staticmethod
-    def setSummary(userId: str, summary: str):
-        vs = milvusDB.vectorstore("UserSummaries")
-        vs.add_texts(texts=[summary], metadatas=[{"chunk_category": userId}])
         
 class mongoDB():
     def getLastConv(id: str, sec: str):
